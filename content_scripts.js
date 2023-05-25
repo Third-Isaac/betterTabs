5d95fc1f8676085882290642d753380d7a63549c
inorle <elinorthw@gmail.com>
2023-05-24 23:43:05 -0400

    made red

--- content_scripts.js
+++ content_scripts.js
@@ -1,4 +1,15 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-  const parentElement = document.querySelector('#parent')
-  parentElement.style.background-color = 'red'
-  })
+// document.addEventListener("DOMContentLoaded", ()=>{
+//   const body1 = document.querySelector('body');
+//   console.log(body1);
+//   body1.style.backgroundColor = 'red';
+// })
+
+const elements = document.querySelectorAll("body");
+elements.push(document.querySelectorAll("head"));
+elements.push(document.querySelectorAll("div"))
+for (let i in elements){
+  elements[i].style.backgroundColor = 'red'
+
+}
+// const body1 = document.querySelector('body');
+// body1.style.backgroundColor = 'red';
