686f604ca04d2294af8f9408af452244f0cec2e4
inorle <elinorthw@gmail.com>
2023-05-25 09:14:51 -0400

    changes to darkmode on arrow down

--- content_scripts.js
+++ content_scripts.js
@@ -6,16 +6,32 @@
 // const image = document.querySelector("body")
 // console.log(image)
 // image.innerHTML = ""
+let keydown = false;
 
-const elements = document.getElementsByTagName("*");
-// document.querySelectorAll("div");
-// elements.push(document.querySelectorAll("head"));
-// elements.push(document.querySelectorAll("div"))
-for (let i in elements){
-  if (elements[i].textContent){ 
-    elements[i].style.color = 'white';
+
+document.addEventListener("keydown", (e) => {
+  keydown = true;
+  if (keydown){
+    // window.location.reload();
+    const elements = document.getElementsByTagName("*");
+    // document.querySelectorAll("div");
+    // elements.push(document.querySelectorAll("head"));
+    // elements.push(document.querySelectorAll("div"))
+    //eve
+    for (let i in elements){
+      if (elements[i].textContent){ 
+        elements[i].style.color = 'white';
+      }
+      elements[i].style.backgroundColor = 'black';
+    }
   }
-  elements[i].style.backgroundColor = 'black';
-}
-// const body1 = document.querySelector('body');
-// body1.style.backgroundColor = 'red';
+});
+
+// document.addEventListener("keyup", (e) => {
+//    if (keydown) {
+//     keydown = false;
+//     window.location.reload();
+// }
+// );
+// // const body1 = document.querySelector('body');
+// // body1.style.backgroundColor = 'red';
